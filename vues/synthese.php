f5cd971e1b7a26ef43b3aec55952d15521fbb0f4
relaxmax54 <maxime.frieh@yahoo.fr>
2014-11-21 08:20:13 +0100

    création formulaire de synthèse

--- vues/synthese.php
+++ vues/synthese.php
@@ -1,20 +1,28 @@
 <div class="col-lg-12">
   <h3>Solde des comptes</h3>
 
-<?php 
+<?php
+
+//création formulaire
+$form = new Form('exemple','POST');
+//on ajoute les champs souhaités
+$form ->add('Submit', 'ok')
+      ->value("Sauvegarder");
+$form ->bound($_POST);
 
 $reponse=Bdd::getInstance()->prepare("SELECT intitule,SUM(montant) as total FROM operations,comptes WHERE operations.compte=comptes.id GROUP BY compte");
 $reponse->execute();
 
 //On parcourt l'objet récupéré sous forme de tableau pour afficher chacun des enregistrements
-echo '<pre>';
-while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC))
-{
-    echo '<pre>';
-    echo '<p>'.print_r($donnees).'</p>';
-    echo '</pre>';
-}
 
+while ($element = $reponse->fetch(PDO::FETCH_ASSOC))
+{
+	$form	->add('Text', $element['intitule'])
+			->value($element['total'])	
+ 	        ->label($element['intitule']);
+}
+//affiche le formulaire
+echo $form;
 
 $reponse->closeCursor();
 
