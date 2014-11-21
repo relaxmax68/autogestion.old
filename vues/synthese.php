9bce4aca2383c33b71adf184f2b30aee08401c1c
relaxmax54 <maxime.frieh@yahoo.fr>
2014-11-21 05:57:58 +0100

    vues: synthese.php affiche le solde des comptes

--- vues/synthese.php
+++ vues/synthese.php
@@ -1,3 +1,22 @@
-<?
-echo "future page de présentation des synthèses";
+<div class="col-lg-12">
+  <h3>Solde des comptes</h3>
+
+<?php 
+
+$reponse=Bdd::getInstance()->prepare("SELECT intitule,SUM(montant) as total FROM operations,comptes WHERE operations.compte=comptes.id GROUP BY compte");
+$reponse->execute();
+
+//On parcourt l'objet récupéré sous forme de tableau pour afficher chacun des enregistrements
+echo '<pre>';
+while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC))
+{
+    echo '<pre>';
+    echo '<p>'.print_r($donnees).'</p>';
+    echo '</pre>';
+}
+
+
+$reponse->closeCursor();
+
 ?>
+</div>
