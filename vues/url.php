9592e333e129f78631cd25639546ef77b2b7d50b
relaxmax54 <maxime.frieh@yahoo.fr>
2014-11-27 15:20:44 +0100

    fix bug redirection vers vue bootstrap.php

--- vues/url.php
+++ vues/url.php
@@ -8,5 +8,5 @@ print_r($_GET);
 echo '<p>*'.$vue.'*</p>';
 echo '<p><a href="'.$application->root.'/comptes/">comptes</a></p>';
 echo '<p><a href="'.$application->root.'/synthese/">synthese</a></p>';
-echo '<p><a href="'.$application->root.'/bootstrap/">testBootstrap</a></p>';
+echo '<p><a href="'.$application->root.'/testBootstrap/">Bootstrap</a></p>';
 echo '<p><a href="'.$application->root.'/saisie/">saisie</a></p>';
